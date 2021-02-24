11e1787faef4b096813c1fa11fb014787ad5c5d2
umarali-nagoor <umarali.nagoor@in.ibm.com>
2021-02-24 15:49:32 +0530

    v13_upgrade: Added required_version argument

--- examples/configure-sysdig-monitor/versions.tf
+++ examples/configure-sysdig-monitor/versions.tf
@@ -7,10 +7,11 @@
 NOTE: To source a particular version of IBM terraform provider, configure the parameter `version` as follows 
 
 terraform {
+  required_version = ">=0.13"
   required_providers {
     ibm = {
       source = "IBM-Cloud/ibm"
-      version = "1.20.0"
+      version = "1.21.0"
     }
   }
 }
@@ -19,6 +20,7 @@ If we dont configure the version parameter, it fetches the latest provider versi
 ****************************************************/
 
 terraform {
+  required_version = ">=0.13"
   required_providers {
     ibm = {
       source = "IBM-Cloud/ibm"
