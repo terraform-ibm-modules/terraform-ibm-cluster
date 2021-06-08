edb13b29e255fc36ffb4e7514bcaf0a9c504a6d6
hkavya26 <hkavya26@in.ibm.com>
2021-06-08 08:50:28 +0530

    Support: Secure ROKS Cluster

---
+++ examples/secure-roks-cluster/cos.tf
@@ -0,0 +1,16 @@
+data "ibm_resource_instance" "cos_instance" {
+  count             = var.cos_instance_name != null ? 1 : 0
+  name              = var.cos_instance_name
+  location          = "global"
+  resource_group_id = data.ibm_resource_group.resource_group.id
+  service           = "cloud-object-storage"
+}
+resource "ibm_resource_instance" "cos_instance" {
+  count             = var.cos_instance_name != null ? 0 : 1
+  name              = "${var.resource_prefix}-cos"
+  location          = "global"
+  resource_group_id = data.ibm_resource_group.resource_group.id
+  service           = "cloud-object-storage"
+  plan              = "standard"
+  tags              = ["FS-Cloud"]
+}
