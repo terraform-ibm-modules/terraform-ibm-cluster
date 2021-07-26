e5e677f8a234587b0608faf1e36ce067cb7cad00
hkavya26 <hkavya26@in.ibm.com>
2021-07-26 19:47:03 +0530

    fix: Activity Tracker

--- examples/secure-roks-cluster/activity-tracker.tf
+++ examples/secure-roks-cluster/activity-tracker.tf
@@ -1,9 +1,16 @@
 resource "ibm_resource_instance" "activity_tracker_instance" {
-  count             = var.activity_tracker_instance_name != null ? 1 : 0
-  name              = var.activity_tracker_instance_name
+  count             = var.activity_tracker_instance_name != null ? 0 : 1
+  name              = "${var.resource_prefix}-at"
   location          = var.region
   resource_group_id = data.ibm_resource_group.resource_group.id
   service           = "logdnaat"
   plan              = "lite"
   tags              = ["secure-roks"]
+}
+data "ibm_resource_instance" "activity_tracker_instance" {
+  count             = var.activity_tracker_instance_name != null ? 1 : 0
+  name              = var.activity_tracker_instance_name
+  service           = "logdnaat"
+  resource_group_id = data.ibm_resource_group.resource_group.id
+  location          = var.region
 }
