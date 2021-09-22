07d99e1cc6fa889c41546e66dcbaf230e9c8cd3b
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-09-22 09:33:52 -0500

    Update versions.tf

--- modules/configure-portworx/versions.tf
+++ modules/configure-portworx/versions.tf
@@ -1,13 +1,7 @@
 # terraform {
-#   required_providers {
-#     ibm = "~> 1.12"
-#   }
-# }
-
-# terraform {
 #   required_version = "v0.12.31"
 #   required_providers {
-#     ibm = "1.28.0"
+#     ibm = "~> 1.12"
 #     kubernetes = "1.13.3"
 #     null = "~> 3.0"
 #   }
