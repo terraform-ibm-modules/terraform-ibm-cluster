788d101ebcb769f9e5385fc8ef80314d8921ed67
soumyadeep-paul-ibm <105362426+soumyadeep-paul-ibm@users.noreply.github.com>
2023-05-25 12:12:08 +0530

    added patch_version

--- modules/vpc-openshift/main.tf
+++ modules/vpc-openshift/main.tf
@@ -20,6 +20,7 @@ resource "ibm_container_vpc_cluster" "cluster" {
   cos_instance_crn                = (var.cos_instance_crn != null ? var.cos_instance_crn : null)
   force_delete_storage            = (var.force_delete_storage != null ? var.force_delete_storage : false)
   entitlement                     = (var.entitlement != null ? var.entitlement : null)
+  patch_version                   = (var.patch_version != null ? var.patch_version : null)
 
   dynamic zones {
     for_each = (var.worker_zones != null ? var.worker_zones : {})
