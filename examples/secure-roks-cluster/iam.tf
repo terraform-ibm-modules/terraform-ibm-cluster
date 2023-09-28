88db7ccb8b99955c64c4aa4884657d2b097bc6ee
Lionel Mace <lionel.mace@gmail.com>
2023-09-28 12:02:57 +0200

    fix iam

--- examples/secure-roks-cluster/iam.tf
+++ examples/secure-roks-cluster/iam.tf
@@ -1,8 +1,17 @@
-module "roks_kms_authorisation_policy" {
-  source              = "terraform-ibm-modules/iam/ibm//modules/service-authorization"
-  version             = "1.2.2"
-  count               = var.roks_kms_policy ? 0 : 1
-  source_service_name = "containers-kubernetes"
-  target_service_name = "kms"
-  roles               = ["Reader"]
+# module "roks_kms_authorisation_policy" {
+#   source              = "terraform-ibm-modules/iam/ibm//modules/service-authorization"
+#   version             = "1.2.2"
+#   count               = var.roks_kms_policy ? 0 : 1
+#   source_service_name = "containers-kubernetes"
+#   target_service_name = "kms"
+#   roles               = ["Reader"]
+# }
+
+# LMA
+resource "ibm_iam_authorization_policy" "kms_authorisation_policy" {
+  count                       = var.roks_kms_policy ? 0 : 1
+  source_service_name         = "containers-kubernetes"
+  source_resource_instance_id = ibm_container_vpc_cluster.iks_cluster.id
+  target_service_name         = "kms"
+  roles                       = ["Reader"]
 }
