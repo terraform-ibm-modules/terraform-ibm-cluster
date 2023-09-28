099ee7c1b078f73279e659c90c3276551e69dd9c
Lionel Mace <lionel.mace@gmail.com>
2023-09-28 12:10:09 +0200

    another kms fix

--- examples/secure-roks-cluster/kms.tf
+++ examples/secure-roks-cluster/kms.tf
@@ -13,13 +13,14 @@
 # }
 
 # LMA
-resource "ibm_resource_instance" kms" {
+resource "ibm_resource_instance" "kms" {
+  count             = var.kms_instance == null && var.kms_key == null ? 1 : 0
   resource_group_id = data.ibm_resource_group.resource_group.id
   name              = "${var.resource_prefix}-kp"
   service           = "kms"
   plan              = "tiered-pricing"
   location          = var.region
-  tags                 = ["secure-roks", var.resource_prefix]
+  tags              = ["secure-roks", var.resource_prefix]
   service_endpoints = "private"
 }
 
